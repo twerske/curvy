85d9e623a72f5226d3884e1d54b63863a4f058e4
vuv915 <emma.twersky@capitalone.com>
2017-08-22 10:18:18 -0400

    make initial hexagon grid

--- js/app.js
+++ js/app.js
@@ -1,62 +1,69 @@
-var $grid = clickableGrid(5, 5);
-$('main').append($grid);
-
-function clickableGrid(rows, cols){
-    var i = 0;
-    var $grid = $('<table>', {
-      class: 'grid'
-    });
-    for (var r = 0; r < rows; ++r){
-        var $tr = $('<tr>');
-        $tr.appendTo($grid);
-        for (var c = 0; c < cols; ++c){
-            var $cell = $('<td>', {
-              id: ++i
-            });
-            $cell.appendTo($tr)
-            $cell.html(i)
-
-            $cell.css('background-image',`url(images/${i%9}.png)`)
-
-            $cell.data('row', r);
-            $cell.data('column', c);
-            $cell.data('angle', 0)
-
-            $cell.on('click', function(){
-                console.log("You clicked on element:", $(this));
-                console.log("You clicked on row:", $(this).data('row'));
-                console.log("You clicked on col:", $(this).data('column'));
-                console.log("You clicked on item #:", $(this).attr('id'));
-
-                rotateCell($(this));
-            });
-        }
-    }
-    return $grid;
-}
-
-function rotateCell($el){
-    var angle = ($el.data('angle') + 90);
-    $el.css({'transform': 'rotate(' + angle + 'deg)'});
-    $el.data('angle', angle);
-}
-
-// $testSVG = $('<svg', {
-//   id: "svg",
-//   width: "0",
-//   height: "0"
-// })
+// var $grid = clickableGrid(5, 5);
+// $('main').append($grid);
 //
-// $testPath = $('<path', {
-//   id: "path",
-//   d: "M0 0"
-// })
-// .appendTo($testSVG)
+// function clickableGrid(rows, cols){
+//     var i = 0;
+//     var $grid = $('<table>', {
+//       class: 'grid'
+//     });
+//     for (var r = 0; r < rows; ++r){
+//         var $tr = $('<tr>');
+//         $tr.appendTo($grid);
+//         for (var c = 0; c < cols; ++c){
+//             var $cell = $('<td>', {
+//               id: ++i
+//             });
+//             $cell.appendTo($tr)
+//             $cell.html(i)
 //
-// function connectAll() {
-//     // connect all the paths you want!
-//     connectElements($("#svg"), $("#path"), $("#1"),  $("#2"));
-//     // ...
-//     // connectElements($("#svg1"), $("#someOtherPath"), $("#purple"), $("#teal")  );
-//     // connectElements($("#svg1"), $("#yetAnotherPath"), $("#Tom"), $("#teal")  );
+//             $cell.css('background-image',`url(images/${i%9}.png)`)
+//
+//             $cell.data('row', r);
+//             $cell.data('column', c);
+//             $cell.data('angle', 0)
+//
+//             $cell.on('click', function(){
+//                 console.log("You clicked on element:", $(this));
+//                 console.log("You clicked on row:", $(this).data('row'));
+//                 console.log("You clicked on col:", $(this).data('column'));
+//                 console.log("You clicked on item #:", $(this).attr('id'));
+//
+//                 rotateCell($(this));
+//             });
+//         }
+//     }
+//     return $grid;
 // }
+//
+
+function rotateHex($el){
+  var angle = ($el.data('angle') + 60) || 60;
+  $el.css({'transform': 'rotate3d(0, 0, 1, ' + angle + 'deg)'});
+  $el.data('angle', angle);
+}
+
+var $hexGrid = $('<ul>', {
+  id: 'hexGrid'
+})
+.appendTo('main');
+
+var gridSize = 30;
+
+for (var i=0; i<gridSize; i++) {
+  var $li = $('<li>', {
+    class: 'hex'
+  })
+  .html(
+    `<div class="hexIn">
+       <a class="hexLink" href="#">
+         <img src="images/${i%9}.png" alt="" />
+       </a>
+     </div>`
+  )
+  .appendTo($hexGrid)
+}
+
+$('.hexLink img').on('click', function(e){
+  e.preventDefault();
+  rotateHex($(this));
+})
