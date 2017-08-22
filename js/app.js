23d652b7011573060947e41843b406ad9afeb3c0
vuv915 <emma.twersky@capitalone.com>
2017-08-22 10:36:22 -0400

    add in actual game images

--- js/app.js
+++ js/app.js
@@ -1,41 +1,3 @@
-// var $grid = clickableGrid(5, 5);
-// $('main').append($grid);
-//
-// function clickableGrid(rows, cols){
-//     var i = 0;
-//     var $grid = $('<table>', {
-//       class: 'grid'
-//     });
-//     for (var r = 0; r < rows; ++r){
-//         var $tr = $('<tr>');
-//         $tr.appendTo($grid);
-//         for (var c = 0; c < cols; ++c){
-//             var $cell = $('<td>', {
-//               id: ++i
-//             });
-//             $cell.appendTo($tr)
-//             $cell.html(i)
-//
-//             $cell.css('background-image',`url(images/${i%9}.png)`)
-//
-//             $cell.data('row', r);
-//             $cell.data('column', c);
-//             $cell.data('angle', 0)
-//
-//             $cell.on('click', function(){
-//                 console.log("You clicked on element:", $(this));
-//                 console.log("You clicked on row:", $(this).data('row'));
-//                 console.log("You clicked on col:", $(this).data('column'));
-//                 console.log("You clicked on item #:", $(this).attr('id'));
-//
-//                 rotateCell($(this));
-//             });
-//         }
-//     }
-//     return $grid;
-// }
-//
-
 function rotateHex($el){
   var angle = ($el.data('angle') + 60) || 60;
   $el.css({'transform': 'rotate3d(0, 0, 1, ' + angle + 'deg)'});
@@ -47,7 +9,7 @@ var $hexGrid = $('<ul>', {
 })
 .appendTo('main');
 
-var gridSize = 30;
+var gridSize = 41;
 
 for (var i=0; i<gridSize; i++) {
   var $li = $('<li>', {
@@ -56,7 +18,7 @@ for (var i=0; i<gridSize; i++) {
   .html(
     `<div class="hexIn">
        <a class="hexLink" href="#">
-         <img src="images/${i%9}.png" alt="" />
+         <img src="images/game1/${i}.png" alt="" />
        </a>
      </div>`
   )
