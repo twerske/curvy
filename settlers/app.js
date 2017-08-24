6d1d5bda8a8eea3328a9ac70699fac8121b9672b
vuv915 <emma.twersky@capitalone.com>
2017-08-23 22:16:31 -0400

    progress on settlers board and setup

--- settlers/app.js
+++ settlers/app.js
@@ -1,60 +1,121 @@
-// When document is ready, begin to create *curvy*
-$(document).ready(function() {
-  // Set size of hex grid (in this case, 6 x 5)
-  var gridSize = 23;
+/*******************************************************************************
+VARIABLES
+*******************************************************************************/
 
+// Set size of hex grid (in this case, 5 x 5)
+var gridSize = 23;
+
+// Array of terrain tiles
+var pieces = ['desert', 'field', 'field', 'field', 'field', 'hill', 'hill',
+'hill', 'mountain', 'mountain', 'mountain', 'pasture', 'pasture',
+'pasture', 'pasture', 'woods', 'woods', 'woods', 'woods'];
+
+var tokens = [[2, '*'], [3, '**'], [3, '**'], [4, '***'], [4, '***'],
+[5, '****'], [5, '****'], [6, '*****'], [6, '*****'], [8, '*****'],
+[8, '*****'], [9, '****'], [9, '****'], [10, '***'], [10, '***'],
+[11, '**'], [11, '**'], [12, '**']];
+
+var resourceCards = {
+  brick: 19,
+  ore: 19,
+  wool: 19,
+  grain: 19,
+  lumber: 19
+}
+
+var developmentCards = {
+  knights: 14,
+  progress: {
+    monopoly: 2,
+    road: 2,
+    yearOfPlenty: 2
+  },
+  victoryPoint: {
+    chapel: 1,
+    library: 1,
+    market: 1,
+    palace: 1,
+    university: 1
+  }
+}
+
+/*******************************************************************************
+                              HELPER FUNCTIONS
+*******************************************************************************/
+// Shuffle an array randomly
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+/*******************************************************************************
+                            WHEN LOADED & LOGIC
+*******************************************************************************/
+$(document).ready(function() {
   // Create hex grid unordered list to hold the hexagons in
   var $hexGrid = $('<ul>', {
     id: 'hexGrid'
   })
   .appendTo('main');
 
-  var pieces = ["desert", "field", "field", "field", "field", "hill", "hill", "hill", "mountain", "mountain", "mountain", "pasture", "pasture", "pasture", "pasture", "woods", "woods", "woods", "woods"];
-  // Create each list item of a hexagonal grid tile and add to the grid
-  for (var i=0; i<gridSize; i++) {
-    if (i != 0 && i != 4 && i != 22 && i != 18) {
-      var src = `images/terrain/${pieces[i]}.png`;
-    }
-    else {src = ""}
-    var $li = $('<li>', {
-      class: 'hex',
-      id: `${i}`
-    })
-    .html(
-      `<div class="hexIn">
-        <a class="hexLink" href="#">
-          <img src=${src} alt="" />
-        </a>
-      </div>`
-    )
-    .appendTo($hexGrid);
+  // Set up game board to play
+  var initializeGameBoard = function() {
+    $('#hexGrid li').remove();
+
+    // Create each list item of a hexagonal grid tile and add to the grid
+    var gamePieces = shuffle(pieces.slice(0));
+    var gameTokens = shuffle(tokens.slice(0));
+    var src, alt, token;
+    for (var i=0; i<gridSize; i++) {
+      if (i == 0 || i == 4 || i == 22 || i == 18) {
+        alt = 'blank';
+        src = `images/terrain/frog.png`;
+        token = [0, ""];
+      }
+      else {
+        alt = gamePieces.pop();
+        src = `images/terrain/${alt}.png`;
+        if (alt == 'desert') {
+          token = ['R', ''];
+        }
+        else {
+          token = gameTokens.pop();
+        };
+      };
+
+      var $li = $('<li>', {
+        class: `hex ${alt}`,
+        id: `token${token[0]}`
+      })
+      .html(
+        `<div class='hexIn ${alt}' >
+          <a class='hexLink' href='#'>
+            <img src=${src} alt=${alt}/>
+            <h1>${token[0]}</h1>
+            <p>${token[1]}</p>
+          </a>
+        </div>`
+      )
+      .appendTo($hexGrid);
+    };
   };
+  initializeGameBoard();
 
-  // var initializeGameBoard = function() {
-  //   var index = 0;
-  //   $('#hexGrid img').each(function() {
-  //     if (index != 0 && index != 4 && index != 22 && index != 18)
-  //     $(this).attr('src', `.images/terrain/${pieces[++index]}.png`);
-  //   });
-  // }
-  // initializeGameBoard();
-
-  // /**
-  // Shuffle each tile on the grid individually and assign this angle of rotation to data angle.
-  // Input: none
-  // Output: no returns, shuffled tiles on the grid
-  // **/
-  // var shuffleGrid = function () {
-  //   for (var i=0; i<gridSize; i++) {
-  //     var random = Math.floor((Math.random() * 6) + 1);
-  //     var angle = 30 + (60 * random);
-  //     $(`#${i}`).data('angle', angle);
-  //     $(`#${i}`).find('img').css({'transform': `rotate3d(0, 0, 1, ${angle}deg)`});
-  //   };
-  // }
-  //
-  // // Shuffle the grid when start is pressed
-  // $('#start').on('click', function(){
-  //   shuffleGrid();
-  // })
+  // Re-initialize game when new game is pressed
+  $('#start').on('click', function(){
+    initializeGameBoard();
+  })
 });
